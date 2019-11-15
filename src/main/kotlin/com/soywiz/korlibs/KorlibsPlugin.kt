2c6d6a2ad1b765afcfb07cc6e554f04c27a18f4b
soywiz <soywiz@gmail.com>
2019-11-15 14:29:59 +0100

    Some adjustments

--- src/main/kotlin/com/soywiz/korlibs/KorlibsPlugin.kt
+++ src/main/kotlin/com/soywiz/korlibs/KorlibsPlugin.kt
@@ -81,7 +81,10 @@ class KorlibsExtension(val project: Project, val nativeEnabled: Boolean, val and
     //val WINDOWS_DESKTOP_NATIVE_TARGETS = listOf("mingwX64", "mingwX86")
     val WINDOWS_DESKTOP_NATIVE_TARGETS = listOf("mingwX64")
     val DESKTOP_NATIVE_TARGETS = LINUX_DESKTOP_NATIVE_TARGETS + MACOS_DESKTOP_NATIVE_TARGETS + WINDOWS_DESKTOP_NATIVE_TARGETS
-    val IOS_TARGETS = listOf("iosArm64", "iosArm32", "iosX64")
+    val BASE_IOS_TARGETS = listOf("iosArm64", "iosArm32", "iosX64")
+	val WATCHOS_TARGETS = listOf("watchosArm64", "watchosArm32", "watchosX64")
+	val TVOS_TARGETS = listOf("tvosArm64", "tvosX64")
+	val IOS_TARGETS = BASE_IOS_TARGETS + WATCHOS_TARGETS + TVOS_TARGETS
     val ALL_NATIVE_TARGETS = IOS_TARGETS + DESKTOP_NATIVE_TARGETS
     val ALL_ANDROID_TARGETS = if (hasAndroid) listOf("android") else listOf()
     val JS_TARGETS = listOf("js")
