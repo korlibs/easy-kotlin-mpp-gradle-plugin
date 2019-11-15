b29a22e7a6e7e78488fcb5ff90bda94e7885fe4a
soywiz <soywiz@gmail.com>
2019-11-15 14:01:30 +0100

    WIP Kotlin 1.3.60

--- src/main/kotlin/com/soywiz/korlibs/targets/Native.kt
+++ src/main/kotlin/com/soywiz/korlibs/targets/Native.kt
@@ -4,112 +4,131 @@ import com.soywiz.korlibs.*
 import org.apache.tools.ant.taskdefs.condition.*
 import org.gradle.api.*
 import org.gradle.api.tasks.*
-import org.gradle.jvm.tasks.Jar
+import org.gradle.jvm.tasks.*
 import org.jetbrains.kotlin.gradle.plugin.*
-import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinTarget
-import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeTest
-import org.jetbrains.kotlin.gradle.tasks.KotlinTest
-import java.io.*
+import org.jetbrains.kotlin.gradle.plugin.mpp.*
+import org.jetbrains.kotlin.gradle.targets.native.tasks.*
 
 val linuxEnabled by lazy { !Os.isFamily(Os.FAMILY_MAC) }
 
 fun Project.configureTargetNative() {
-    val nativeExtraJar = tasks.create<Jar>("nativeExtraJar") {
-    }
+	val nativeExtraJar = tasks.create<Jar>("nativeExtraJar") {
+	}
 
-    fun AbstractKotlinTarget.extraNative() {
-        mavenPublication(Action { it.artifact(nativeExtraJar) })
+	fun AbstractKotlinTarget.extraNative() {
+		mavenPublication(Action { it.artifact(nativeExtraJar) })
 		this.attributes.attribute(KotlinPlatformType.attribute, KotlinPlatformType.native)
-    }
+	}
 
-    gkotlin.apply {
-        iosX64() {
-            extraNative()
-        }
-        iosArm32() {
-            extraNative()
-        }
-        iosArm64() {
-            extraNative()
-        }
-        macosX64() {
-            extraNative()
-        }
+	gkotlin.apply {
+		/////////////////////////////////////////
+		iosX64() {
+			extraNative()
+		}
+		iosArm32() {
+			extraNative()
+		}
+		iosArm64() {
+			extraNative()
+		}
+		/////////////////////////////////////////
+		tvosX64() {
+			extraNative()
+		}
+		tvosArm64() {
+			extraNative()
+		}
+		/////////////////////////////////////////
+		watchosX86() {
+			extraNative()
+		}
+		watchosArm32() {
+			extraNative()
+		}
+		watchosArm64() {
+			extraNative()
+		}
+		/////////////////////////////////////////
+		macosX64() {
+			extraNative()
+		}
 		if (linuxEnabled) {
 			linuxX64() {
 				extraNative()
 			}
 		}
-        mingwX64() {
-            extraNative()
-        }
+		mingwX64() {
+			extraNative()
+		}
 
-        if (System.getProperty("idea.version") != null) {
-            when {
-                Os.isFamily(Os.FAMILY_WINDOWS) -> run { mingwX64("nativeCommon"); mingwX64("nativePosix") }
-                Os.isFamily(Os.FAMILY_MAC) -> run { macosX64("nativeCommon"); macosX64("nativePosix") }
-                else -> run {
-					if (linuxEnabled) { linuxX64("nativeCommon"); linuxX64("nativePosix") }
+		if (System.getProperty("idea.version") != null) {
+			when {
+				Os.isFamily(Os.FAMILY_WINDOWS) -> run { mingwX64("nativeCommon"); mingwX64("nativePosix") }
+				Os.isFamily(Os.FAMILY_MAC) -> run { macosX64("nativeCommon"); macosX64("nativePosix") }
+				else -> run {
+					if (linuxEnabled) {
+						linuxX64("nativeCommon"); linuxX64("nativePosix")
+					}
 				}
-            }
-        }
+			}
+		}
 
-        sourceSets.apply {
-            fun dependants(name: String, on: Set<String>) {
-                val main = maybeCreate("${name}Main")
-                val test = maybeCreate("${name}Test")
-                for (o in on) {
-                    maybeCreate("${o}Main").dependsOn(main)
-                    maybeCreate("${o}Test").dependsOn(test)
-                }
-            }
+		sourceSets.apply {
+			fun dependants(name: String, on: Set<String>) {
+				val main = maybeCreate("${name}Main")
+				val test = maybeCreate("${name}Test")
+				for (o in on) {
+					maybeCreate("${o}Main").dependsOn(main)
+					maybeCreate("${o}Test").dependsOn(test)
+				}
+			}
 
-            val none = setOf<String>()
-            val android = if (hasAndroid) setOf() else setOf("android")
-            val jvm = korlibs.JVM_TARGETS.toSet()
-            val js = korlibs.JS_TARGETS.toSet()
-            val ios = korlibs.IOS_TARGETS.toSet()
-            val macos = korlibs.MACOS_DESKTOP_NATIVE_TARGETS.toSet()
-            val linux = korlibs.LINUX_DESKTOP_NATIVE_TARGETS.toSet()
-            val mingw = korlibs.WINDOWS_DESKTOP_NATIVE_TARGETS.toSet()
-            val apple = ios + macos
-            val allNative = apple + linux + mingw
-            val jvmAndroid = jvm + android
-            val allTargets = allNative + js + jvm + android
+			val none = setOf<String>()
+			val android = if (hasAndroid) setOf() else setOf("android")
+			val jvm = korlibs.JVM_TARGETS.toSet()
+			val js = korlibs.JS_TARGETS.toSet()
+			val ios = korlibs.IOS_TARGETS.toSet()
+			val macos = korlibs.MACOS_DESKTOP_NATIVE_TARGETS.toSet()
+			val linux = korlibs.LINUX_DESKTOP_NATIVE_TARGETS.toSet()
+			val mingw = korlibs.WINDOWS_DESKTOP_NATIVE_TARGETS.toSet()
+			val apple = ios + macos
+			val allNative = apple + linux + mingw
+			val jvmAndroid = jvm + android
+			val allTargets = allNative + js + jvm + android
 
-            dependants("iosCommon", ios)
-            dependants("nativeCommon", allNative)
-            dependants("nonNativeCommon", allTargets - allNative)
-            dependants("nativePosix", allNative - mingw)
-            dependants("nativePosixNonApple", allNative - mingw - apple)
-            dependants("nativePosixApple", apple)
-            dependants("nonJs", allTargets - js)
-        }
-    }
+			dependants("iosCommon", ios)
+			dependants("nativeCommon", allNative)
+			dependants("nonNativeCommon", allTargets - allNative)
+			dependants("nativePosix", allNative - mingw)
+			dependants("nativePosixNonApple", allNative - mingw - apple)
+			dependants("nativePosixApple", apple)
+			dependants("nonJs", allTargets - js)
+		}
+	}
 
-    afterEvaluate {
-        for (target in korlibs.DESKTOP_NATIVE_TARGETS) {
-            val taskName = "copyResourcesToExecutable_$target"
-            val targetTestTask = tasks.getByName("${target}Test") as KotlinNativeTest
-            val compileTestTask = tasks.getByName("compileTestKotlin${target.capitalize()}")
-            val compileMainTask = tasks.getByName("compileKotlin${target.capitalize()}")
+	afterEvaluate {
+		for (target in korlibs.DESKTOP_NATIVE_TARGETS) {
+			val taskName = "copyResourcesToExecutable_$target"
+			val targetTestTask = tasks.getByName("${target}Test") as KotlinNativeTest
+			val compileTestTask = tasks.getByName("compileTestKotlin${target.capitalize()}")
+			val compileMainTask = tasks.getByName("compileKotlin${target.capitalize()}")
 
-            tasks {
-                create<Copy>(taskName) {
-                    for (sourceSet in gkotlin.sourceSets) {
-                        from(sourceSet.resources)
-                    }
+			tasks {
+				create<Copy>(taskName) {
+					for (sourceSet in gkotlin.sourceSets) {
+						from(sourceSet.resources)
+					}
 
-                    into(targetTestTask.executable.parentFile)
-                }
-            }
+					into(targetTestTask.executable.parentFile)
+				}
+			}
 
-            targetTestTask.inputs.files(
-                *compileTestTask.outputs.files.files.toTypedArray(),
-                *compileMainTask.outputs.files.files.toTypedArray()
-            )
+			targetTestTask.inputs.files(
+				*compileTestTask.outputs.files.files.toTypedArray(),
+				*compileMainTask.outputs.files.files.toTypedArray()
+			)
 
-            targetTestTask.dependsOn(taskName)
-        }
-    }
+			targetTestTask.dependsOn(taskName)
+		}
+	}
 }
