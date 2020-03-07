760affa74b4f0116784533c7850d152658e0ddc3
soywiz <soywiz@gmail.com>
2020-03-07 12:41:06 +0100

    Copy javascript resources to test folder

--- src/main/kotlin/com/soywiz/korlibs/targets/JavaScript.kt
+++ src/main/kotlin/com/soywiz/korlibs/targets/JavaScript.kt
@@ -3,7 +3,9 @@ package com.soywiz.korlibs.targets
 import com.moowork.gradle.node.*
 import com.soywiz.korlibs.*
 import org.gradle.api.*
+import org.gradle.api.tasks.*
 import org.jetbrains.kotlin.gradle.plugin.*
+import org.jetbrains.kotlin.gradle.tasks.*
 
 fun Project.configureTargetJavaScript() {
 	(project.extensions.getByName("node") as NodeExtension).apply {
@@ -38,7 +40,34 @@ fun Project.configureTargetJavaScript() {
         }
     }
 
-    dependencies.apply {
+	afterEvaluate {
+		for (target in korlibs.JS_TARGETS) {
+			val taskName = "copyResourcesToExecutable_$target"
+			val targetTestTask = tasks.findByName("${target}Test") as? Kotlin2JsCompile? ?: continue
+			val compileTestTask = tasks.findByName("compileTestKotlin${target.capitalize()}") ?: continue
+			val compileMainTask = tasks.findByName("compileKotlin${target.capitalize()}") ?: continue
+
+			tasks {
+				create<Copy>(taskName) {
+					for (sourceSet in gkotlin.sourceSets) {
+						from(sourceSet.resources)
+					}
+
+					into(targetTestTask.outputFile.parentFile.parentFile)
+				}
+			}
+
+			targetTestTask.inputs.files(
+				*compileTestTask.outputs.files.files.toTypedArray(),
+				*compileMainTask.outputs.files.files.toTypedArray()
+			)
+
+			targetTestTask.dependsOn(taskName)
+		}
+	}
+
+
+	dependencies.apply {
         add("jsMainImplementation", "org.jetbrains.kotlin:kotlin-stdlib-js")
         add("jsTestImplementation", "org.jetbrains.kotlin:kotlin-test-js")
     }
