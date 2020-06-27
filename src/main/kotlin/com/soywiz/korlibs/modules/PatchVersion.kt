251a0a66728673fae6ba8bf6acd0507410ec9b78
soywiz <soywiz@gmail.com>
2020-06-27 21:44:16 +0200

    FORCED_KORGE_PLUGINS_VERSION -> FORCED_VERSION

--- src/main/kotlin/com/soywiz/korlibs/modules/PatchVersion.kt
+++ src/main/kotlin/com/soywiz/korlibs/modules/PatchVersion.kt
@@ -4,7 +4,7 @@ import org.gradle.api.*
 
 fun Project.configurePatchVersion() {
 	allprojects {
-		val forcedVersion = System.getenv("FORCED_KORGE_PLUGINS_VERSION")
+		val forcedVersion = System.getenv("FORCED_VERSION")
 		project.version = forcedVersion?.removePrefix("refs/tags/v")?.removePrefix("v") ?: project.version
 	}
 }
