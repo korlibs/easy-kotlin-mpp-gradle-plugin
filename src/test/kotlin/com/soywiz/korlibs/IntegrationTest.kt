4ea6f6d28eabf213f11375a77fe40c320b0ebc0f
soywiz <soywiz@gmail.com>
2021-03-10 22:03:44 +0100

    Add initial basic IntegrationTest

---
+++ src/test/kotlin/com/soywiz/korlibs/IntegrationTest.kt
@@ -0,0 +1,20 @@
+package com.soywiz.korlibs
+
+import org.gradle.testfixtures.*
+import org.junit.*
+import org.junit.rules.*
+
+class IntegrationTest {
+	val tempFolder = TemporaryFolder().apply { create() }
+	@After
+	fun deleteTempFolder() = tempFolder.delete()
+
+	val project = ProjectBuilder.builder().withProjectDir(tempFolder.root).build().also {
+		println("INITIALIZED PROJECT AT ${it.rootDir}")
+	}
+
+	@Test
+	fun testApplyNoExceptionThrown() {
+		project.plugins.apply(KorlibsPlugin::class.java)
+	}
+}
